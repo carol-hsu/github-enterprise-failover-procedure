345e4dedf36cfe49b4de289ed871a415e1416980
carol-hsu <asymplone@gmail.com>
2015-04-27 16:53:55 +0800

    first commit

--- ghe_primary_to_replica.sh
+++ ghe_primary_to_replica.sh
@@ -1,29 +1,15 @@
 #! /bin/bash
 
-ReplicaIP=$1
+OldPrimaryIP=$1
+
+NewPrimaryIP=$2
 
 SSHKeyFile="/Users/carol/Carol_key_California.pem"
 
-ReplicaOK=""
 
+#get RSA keypair
 
-function VerifyStatus {
-	VerifyOK=$(ssh -i $SSHKeyFile -p 122 admin@$ReplicaIP "ghe-repl-status -vv" | grep "OK:" | wc -l)
-	if ( $VerifyOK = "4"){
-		ReplicaOK="1"
-	}else{
-		ReplicaOK="0"
-	}
-}
+RSAValue=$(ssh -i $SSHKeyFile -p 122 admin@$OldPrimaryIP "ghe-repl-setup $NewPrimaryIP"| grep "ssh-rsa")
 
-#wait 30 seconds
-sleep 3
-
-#verify the status of replica node is fine
-VerifyStatus
-
-#change replica node to be primary
-
-ssh -i $SSHKeyFile -p 122 admin@$ReplicaIP "ghe-repl-promote"
-
+echo $RSAValue
 
