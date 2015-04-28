2f977d1d044e00eeaa74ca1c33d5e3c3b3f2e0f1
carol-hsu <asymplone@gmail.com>
2015-04-28 16:44:17 +0800

    inital setup

--- ghe_initial_setup.py
+++ ghe_initial_setup.py
@@ -3,15 +3,62 @@
 import sys, os
 
 PrimaryIP = sys.argv[1]
-
 ReplicaIP = sys.argv[2]
+LicenseFile = sys.argv[3]
 
 SSHKeyFile = "/Users/carol/Carol_key_California.pem"
 
+SSHPrimary = "ssh -i "+SSHKeyFile+" -p 122 admin@"+PrimaryIP
+SSHReplica = "ssh -i "+SSHKeyFile+" -p 122 admin@"+ReplicaIP
 
-#get RSA keypair
-os.system("echo 5678")
-os.system('RSAValue=$(ssh -i ' + SSHKeyFile + ' -p 122 admin@' + ReplicaIP + ' "ghe-repl-setup ' + PrimaryIP + '"| grep "ssh-rsa")')
-os.system("echo 1234")
-os.system("echo $RSAValue")
+#----- set hostname ----- 
+os.system(SSHReplica + " \"echo '127.0.0.1 ip-" + "-".join(ReplicaIP.split("."))+"' | sudo tee -a /etc/hosts\"")
+os.system(SSHReplica + " \"echo '"+PrimaryIP+" ip-" + "-".join(PrimaryIP.split("."))+"' | sudo tee -a /etc/hosts\"")
+os.system(SSHPrimary + " \"echo '127.0.0.1 ip-" + "-".join(PrimaryIP.split("."))+"' | sudo tee -a /etc/hosts\"")
+os.system(SSHPrimary + " \"echo '"+ReplicaIP+" ip-" + "-".join(ReplicaIP.split("."))+"' | sudo tee -a /etc/hosts\"")
+
+#----- start github service on primary node -----
+#import license, set configuration password, and apply setting
+os.system("cat "+LicenseFile+" | "+SSHPrimary+" -- ghe-import-license")
+os.system(SSHPrimary+" \"ghe-set-password\"")
+os.system(SSHPrimary+" \"ghe-config-apply\"")
+
+#----- RSA Keypair setting for Replica setup -----
+#get RSA keypair and dump it in a file
+os.system(SSHReplica+" \"ghe-repl-setup "+PrimaryIP+" | grep 'ssh-rsa' \" > replicaKey")
+#copy the file to Primary node
+os.system("scp -i "+SSHKeyFile+" -P 122 replicaKey admin@"+PrimaryIP+":/home/admin/")
+#import the key into Primary node's authentication
+os.system(SSHPrimary+" \"ghe-import-authorized-keys < replicaKey\"")
+
+#----- Replica node setup -----
+#initail
+os.system(SSHReplica+" \"ghe-repl-setup "+PrimaryIP+"\"")
+#Start replica service
+os.system(SSHReplica+" \"ghe-repl-start\"")
+#Check replica status
+os.system(SSHReplica+" \"ghe-repl-status\"")
+
+'''
+print (SetHostCommand)
+print (LicenseCommand)
+print (KeyCommand)
+print (CopyCommand)
+print (ImportCommand)
+print (SetupCommand)
+print (StartReplicaCommand)
+print (ReplicaStatusCommand)
+'''
+#os.system(KeyCommand)
+#os.system(CopyCommand)
+#os.system(ImportCommand)
+
+
+#KeyProcess = subprocess.Popen(KeyPairCommand,shell=True,
+#				 			  stdout=subprocess.PIPE, 
+#				 			  stderr=subprocess.PIPE )
+
+#out, error = KeyProcess.communicate()
+#errcode = KeyProcess.returncode
+#RSAKey = out.decode('utf8')[:-1]
 
